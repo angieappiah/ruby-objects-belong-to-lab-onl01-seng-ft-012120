857344c277ccb41fc2603765ef84416e593f8f0d
Angela Appiah <alegnalawn2@yahoo.com>
2020-02-05 04:01:35 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -5,22 +5,6 @@ class Artist
   def initialize (name)
     @name = name
   end
-  
-  def add_song(song)
-    @song << song
-    song.artist = self
-    @@song_count +=1 
-  end
-  
-  def add_song_by_name (name)
-    song = song.new (name)
-    @song << song
-    song.artist = self
-    @@song_count += 1 
-  end
-  
-  def self.song_count
-    @@song_count
-  end
-end
+end 
+ 
   
